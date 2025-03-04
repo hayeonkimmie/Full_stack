7904f3e9e3e857892d80bf1fa0fdfebac8163139
Hayeon <124011034+hayeonkimmie@users.noreply.github.com>
2025-03-04 23:50:08 +0900

    Add files via upload

---
+++ RandomTest.java
@@ -0,0 +1,10 @@
+package src.loop;
+
+public class RandomTest {
+
+	public static void main(String[] args) {
+		int rand = (int) (Math.random()*45)+1;
+		System.out.println(rand);
+		System.out.println(Math.random());
+	}
+}
